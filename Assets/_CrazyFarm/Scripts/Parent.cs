73eecca1f2f306e24a31f8107b7201eb1fcb64ba
AnaelleMncs <amencias@smart-tale.com>
2024-02-23 15:54:42 +0100

    [ADD][Reject and dispawn method]

--- Assets/_CrazyFarm/Scripts/Parent.cs
+++ Assets/_CrazyFarm/Scripts/Parent.cs
@@ -16,23 +16,27 @@ namespace SheepFold
 
 		private void Start()
 		{
-			//currentBabies = 
+			currentBabies = new List<Baby>();
 			targetBabies = Random.Range(minBabies, maxBabies);
 		}
 
 		private void OnTriggerEnter2D(Collider2D collision)
 		{
 			//s'abonne au OnRelease du bébé
+			/*if (collision.gameObject.TryGetComponent<Baby>(out Baby lBaby)
+				lBaby.*/
 		}
 
 		private void OnTriggerExit2D(Collider2D collision)
 		{
 			//se désabonne au OnRelease du bébé
+			/*if (collision.gameObject.TryGetComponent<Baby>(out Baby lBaby)
+				lBaby.*/
 		}
 
 		private void CheckRightBaby(Baby baby)
 		{
-			/*if (baby.animalsInfo.type == animalsInfo.type)
+			if (baby.type == type)
 			{
 				Debug.Log("It's the right baby");
 				currentBabies.Add(baby);
@@ -43,22 +47,21 @@ namespace SheepFold
 			else
 			{
 				Debug.Log("It's the wrong baby");
-				RejectBaby();
-			}*/
+				RejectBaby(baby);
+			}
 		}
 
-		private void RejectBaby()
+		private void RejectBaby(Baby baby)
 		{
-
+			baby.gameObject.transform.position = Enclosure.Instance.Bounds.RandomPoint();
 		}
 
 		private void Despawn()
 		{
-			foreach (Baby lBaby in currentBabies)
-			{
-				Destroy(lBaby.gameObject);
-				Destroy(this);
-			}
+			for (int i = currentBabies.Count - 1; i >= 0; i--)
+				Destroy(currentBabies[i].gameObject);
+
+			Destroy(this);
 		}
 	}
 }
