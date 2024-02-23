c3757e86130fbe07bde41270928195439dd2f4e6
Gabriel Bernabeu <gabrielbernabeuetude@gmail.com>
2024-02-23 15:40:24 +0100

    [UPD] [To old input system]

---
+++ Assets/_CrazyFarm/Scripts/InactiveFeedback.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace SheepFold
+{
+    public class InactiveFeedback : MonoBehaviour
+    {
+        [SerializeField] private float inactiveDuration;
+
+        private void Update()
+        {
+            //if (Touch)
+        }
+    }
+}
